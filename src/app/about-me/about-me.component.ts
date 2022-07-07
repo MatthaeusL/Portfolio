b1f936981c839e50b3c48f386006f87f17d046b2
MatthaeusL <mat@lispage.com>
2022-07-07 16:51:53 +0200

    update content

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -11,22 +11,22 @@ export class AboutMeComponent implements OnInit {
     {
       AMicon: 'assets/img/walk-line.png',
       AMtitle: 'MY JURNEY BEGAN',
-      AMtext: 'as a product manager in an industrial company. Early on, I got the chance to collaborate in multiple IT and digitalization projects. as a product manager in an industrial company. Early on, I got the chance to collaborate in multiple IT and digitalization projects',
+      AMtext: 'My journey into the world of technology started early. I started working with macromedia flash at the young age of 16 and later as a student (where I worked for the international geothermal centre). I gained knowledge by educating myself in labview. Later as an entrepreneur / founder of a medical transport company my daily business was to organize the administration and handle documentation. For more efficiency in our daily operations I worked with Microsoft Office automation.',
     },
     {
       AMicon: 'assets/img/heart-line.png',
-      AMtitle: 'My love for IT was born',
-      AMtext: 'as a product manager in an industrial company. Early on, I got the chance to collaborate in multiple IT and digitalization projects.',
+      AMtitle: 'I was always fascinated',
+      AMtext: 'by technology. The process of creating a code from the very beginning to a final direct solution always inspired me and accompanies me along the path of my career. So it was just a matter of time before I would turn my passion into a profession.',
     },
     {
       AMicon: 'assets/img/search-line.png',
       AMtitle: 'Here I found what I was looking for',
-      AMtext: 'as a product manager in an industrial company. Early on, I got the chance to collaborate in multiple IT and digitalization projects.',
+      AMtext: 'To make progress and gain a lot of practical experience in software development I got professional training as a front end developer at the Developer Academy. With their systematic training I am now able to build websites and apps in teams and also alone and got professional code reviews.',
     },
     {
       AMicon: 'assets/img/flight-takeoff-line.png',
-      AMtitle: 'Now my journey starts',
-      AMtext: 'as a product manager in an industrial company. Early on, I got the chance to collaborate in multiple IT and digitalization projects.',
+      AMtitle: 'My new journey begins ',
+      AMtext: 'as a frontend developer i would apply my experience and looking for a new challange.',
     },
   ]
   constructor() { }
