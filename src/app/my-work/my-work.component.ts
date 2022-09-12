2c1ecb19d62b83cc3334b85ff7dbf91af88f13f6
MatthaeusL <mat@lispage.com>
2022-09-12 19:08:00 +0200

    responsive img change

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -8,18 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class MyWorkComponent implements OnInit {
   buttons = [
     { name: 'All', set: true },
-    { name: 'Java script', set: false },
+    { name: 'JavaScript', set: false },
     { name: 'Angular', set: false },
 
 
   ];
+  // projectcode  => 0=all > 1=JavaScript > 2=Angular
   projects = [
     {
       projID: 0,
       projName: 'elpolloloco',
       projDescription: 'JavaScript besed jump amd run game',
       projPic: 'assets/img/proj/elpolloloco.png',
-      projLink: 'http://demate.bplaced.net',
+      projLink: 'https://matlis.info/elpolloloco/index.html',
       projGitLink: 'https://github.com/MatthaeusL/elpolloloco',
       projCode: [0, 1]
     },
@@ -28,14 +29,14 @@ export class MyWorkComponent implements OnInit {
       projName: 'Join',
       projDescription: 'JavaScript besed Kanban board',
       projPic: 'assets/img/proj/join.png',
-      projLink: 'http://www.gruppe-161.developerakademie.net/index.html',
+      projLink: 'https://matlis.info/join/index.html',
       projGitLink: 'https://github.com/MatthaeusL/Kanban-Gruppenarbeit--161',
       projCode: [0, 1]
     },
     {
       projID: 2,
       projName: 'ring of fire',
-      projDescription: 'Angular based game ',
+      projDescription: 'Angular based game',
       projPic: 'assets/img/proj/ROF.png',
       projLink: 'https://ring-of-fire-15e5c.web.app',
       projGitLink: 'https://github.com/MatthaeusL/RingOfFire',
@@ -45,20 +46,20 @@ export class MyWorkComponent implements OnInit {
       projID: 3,
       projName: 'Portfolio',
       projDescription: 'Angular besed portfolio with my skills an projects',
-      projPic: 'assets/img/proj/ROF.png',
+      projPic: 'assets/img/proj/portfolio.png',
       projLink: 'http://matlis.info',
       projGitLink: 'https://github.com/MatthaeusL/Portfolio',
       projCode: [0, 2]
     },
-    {
-      projID: 4,
-      projName: 'pokedex',
-      projDescription: 'REST Api based Site',
-      projPic: 'assets/img/proj/elpolloloco.png',
-      projLink: 'http://demate.bplaced.net',
-      projGitLink: 'http://google.de',
-      projCode: [0, 1]
-    },
+    // {
+    //   projID: 4,
+    //   projName: 'pokedex',
+    //   projDescription: 'REST Api based Site',
+    //   projPic: 'assets/img/proj/elpolloloco.png',
+    //   projLink: 'http://demate.bplaced.net',
+    //   projGitLink: 'http://google.de',
+    //   projCode: [0, 1]
+    // },
 
   ]
   j: number;
